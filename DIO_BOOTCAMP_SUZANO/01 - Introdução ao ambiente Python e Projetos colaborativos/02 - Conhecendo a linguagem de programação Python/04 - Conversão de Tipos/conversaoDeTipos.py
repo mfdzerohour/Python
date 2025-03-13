3850aedc03dafc14afe38fa831f90f2cd91a3b8b
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-13 17:46:36 -0400

    Convertendo Tipos

---
+++ DIO_BOOTCAMP_SUZANO/01 - Introdução ao ambiente Python e Projetos colaborativos/02 - Conhecendo a linguagem de programação Python/04 - Conversão de Tipos/conversaoDeTipos.py
@@ -0,0 +1,43 @@
+#Tipo inteiro
+preco = 10
+print(preco)
+
+#convertendo inteiro para float
+preco = float(preco)
+print(preco)
+
+#a divisão de dois inteiros gera um float
+preco = 50 / 2
+print(preco)
+
+#convertendo float para inteiro
+preco = int(preco)
+print(preco)
+
+#Detalhando a divisão para ter float ou não
+preco = 10 / 3
+print(preco) # 3.3333
+
+#Se utilizar duas // ele deixa só o inteiro descartando o ponto flutuante
+preco = 10 // 3
+print(preco) # 3
+
+### CONVERTEBDO numero para string
+preco = 10.50
+idade = 28
+
+#usa o STR para converter para string
+print (str(preco))
+
+print (str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+#de STRING para NUMEROS
+preco = "10.50"
+idade = "28"
+
+print(float(preco))
+
+print(int(idade))
