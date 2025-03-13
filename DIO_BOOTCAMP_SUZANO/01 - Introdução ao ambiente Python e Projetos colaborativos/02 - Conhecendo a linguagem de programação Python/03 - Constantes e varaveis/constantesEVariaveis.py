7444475ea537684b5b81131881abe9e3bd513d9c
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-13 17:32:46 -0400

    Como usar constantes e variaveis

--- DIO_BOOTCAMP_SUZANO/01 - Introdução ao ambiente Python e Projetos colaborativos/02 - Conhecendo a linguagem de programação Python/03 - Constantes e varaveis/constantesEVariaveis.py
+++ DIO_BOOTCAMP_SUZANO/01 - Introdução ao ambiente Python e Projetos colaborativos/02 - Conhecendo a linguagem de programação Python/03 - Constantes e varaveis/constantesEVariaveis.py
@@ -12,3 +12,14 @@ print(f'Menu nome é {name} e tenho {age} ano(s) de idade.')
 
 age, name = (6, 'Yasmin' )
 print(f'Menu nome é {name} e tenho {age} ano(s) de idade.')
+
+# Por convenção a palavra toda em MAIUSCULA não pode modificar seu conteudo
+ABS_PATH='C:\Desenvolvimento'
+DEBUG=True
+STATES = [
+    'SP',
+    'RJ',
+    'MS',
+    'MT'
+]
+AMOUNT = 30.2
