397250308d7ad42ff894c445b8e4ce25c91d99f4
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-17 17:34:33 -0400

    operadores de compatação

---
+++ DIO_BOOTCAMP_SUZANO/02_-_Sintaxe_Basica_Python/02_-_Operadores_de_Comparação/01_-_Operadores_de_Comparacao.py
@@ -0,0 +1,9 @@
+saldo = 450
+saque = 200
+
+print (saldo == saque) # False
+print (saldo != saque) # True
+print (saldo >= saque) # True
+print (saldo <= saque) 
+print (saldo < saque) 
+print (saldo > saque) 
