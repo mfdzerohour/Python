59f13f0f99244e2a93d1b04144f7897bf311d445
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-17 17:28:07 -0400

    Procedencia de Operadores matematicos

---
+++ DIO_BOOTCAMP_SUZANO/02_-_Sintaxe_Basica_Python/01_-_Tipos_de_Operadores/01_-_Operadores_Arientimeticos.py
@@ -0,0 +1,73 @@
+#Adição
+print (1 + 1)
+# 2
+
+#Subtração
+print (10 - 2)
+# 8
+
+#Multiplicação
+print (4 * 3)
+# 12
+
+#Divisão
+print (10 / 2)
+# 5
+
+#Divisão
+print (12 // 2)
+# 6
+
+#Módulo ou Resto da Divisão
+print (10 % 3)
+# 1
+
+#Exponenciação
+print (2 ** 3)
+
+#Procedencia Matematica
+# A definição indica a seguinte ordem como a correta:
+# 1 - Parenteses
+# 2 - Expoêntes
+# 3 - Multiplicação e divisões (da esquerda para que direita)
+# 4 - Adição e subtração (da esquerda para que direita)
+
+print (10 - 5 * 2) #Ordem da matematica
+# 0
+
+print ((10 - 5) * 2) #Parenteses
+# 8 
+
+print (10 ** 2 * 2)
+# 200 é o mesmo que 10 * 4 = 200
+
+print (10 ** (2 * 2))
+# 10.000 é o mesmo que 10 * 10 * 10 * 10 = 10000
+
+print(10 / 2 * 4);
+# 20.0    
+
+# ******************************************************************************** #
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2)
+# 30
+
+print(produto1 - produto2)
+# -10
+
+print(produto1 * produto2)
+# 200
+
+print(produto1 / produto2)
+# 0.5
+
+print(produto1 // produto2)
+# 0
+
+print(produto1 % produto2)
+# 10
+
+print(produto1 ** produto2) 
+# 100.000.000.000.000.000.000
