946a61ced5c5fbdd90f5b10fb03e5fda49f33d19
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-24 17:53:18 -0400

    Iniciando o programa em python do banco

---
+++ DIO_BOOTCAMP_SUZANO/02_-_Sintaxe_Basica_Python/05_-_Desafio_de_Projeto/01_-_Criando_um_Sistema_Bancário_Com_Python/02_-_Entendendo_o_Desafio/01_-_prjBancario.py
@@ -0,0 +1,34 @@
+saldo = int(input("Informe o saldo que possui na conta: "))
+
+print("""################
+1 - Saque
+2 - Deposito
+3 - Saldo
+0 - Sair
+################
+"""
+)
+
+limite = saldo * 0.30
+extrato = **
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = int(input("Informe a opção desejada: "))
+
+    #Saque
+    if opcao == 1:
+    
+    #Deposito
+    elif opcao == 2:
+    
+    #Saldo
+    elif opcao == 3:
+    
+    #Sair
+    elif opcao == 0:
+
+    #Opcao invalida
+    else:
+        print("Opção inválida")
