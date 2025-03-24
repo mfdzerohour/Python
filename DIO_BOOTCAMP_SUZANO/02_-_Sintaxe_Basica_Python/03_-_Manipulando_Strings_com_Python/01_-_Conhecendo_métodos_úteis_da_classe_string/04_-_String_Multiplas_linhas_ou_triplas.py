989bdb55667e80a8649d0a752d9343b8b33e7551
mfdzerohour <mfdzerohour@hotmail.com>
2025-03-24 08:52:06 -0400

    Adicionado o exemplo de como usar uma string de multiplas linhas e informações de referencias a serem consultadas posteriromente,

---
+++ DIO_BOOTCAMP_SUZANO/02_-_Sintaxe_Basica_Python/03_-_Manipulando_Strings_com_Python/01_-_Conhecendo_métodos_úteis_da_classe_string/04_-_String_Multiplas_linhas_ou_triplas.py
@@ -0,0 +1,33 @@
+nome = "Marcelo"
+
+print()
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+"""
+
+print(mensagem)
+
+print()
+
+mensagem = f'''
+    Olá meu nome é {nome},
+  Eu estou aprendendo Python
+        Essa mensagem tem diferentes recuos.
+'''
+
+print(mensagem)
+
+print()
+
+print(''''
+      ****** MENU ******
+      * 1 - Depositar  *
+      * 2 - Sacar      *
+      * 3 - Saldo      *
+      * 4 - Sair       *
+      ****** MENU ******
+      Obrigado, por usar
+       nosso sistema!
+''')
